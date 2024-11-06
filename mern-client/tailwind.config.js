6289b0b497e84ef696dacaa90ce0850e268995a7
ShilpiBiswal <biswalshilpi@gmail.com>
2024-11-06 14:57:10 +0530

    upload book

--- mern-client/tailwind.config.js
+++ mern-client/tailwind.config.js
@@ -1,6 +1,8 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    flowbite.content(),
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
