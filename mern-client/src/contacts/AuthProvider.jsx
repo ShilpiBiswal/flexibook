4de87e68d8e0559d4d245977c6cec162dc42c335
BunnyBrewster1724 <shashidharshivani88@gmail.com>
2024-11-08 00:22:19 +0530

    Update AuthProvider.jsx

--- mern-client/src/contacts/AuthProvider.jsx
+++ mern-client/src/contacts/AuthProvider.jsx
@@ -13,7 +13,7 @@ const googleProvider = new GoogleAuthProvider();
 
 
 const AuthProvider = ({children}) => {
-    const [user, setuser] = useState(null);
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
